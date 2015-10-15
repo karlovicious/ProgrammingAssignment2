14c0e147f748569b2c661f2f092c86c4ac3a6138
karlovicious <karlovicj@gmail.com>
2015-10-15 16:23:34 -0700

    final version of function
    Final version of the matrix caching/solving function. Ready for
    submission and evaluation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+# function to create and cache a matrix using a
+# different environment
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function (x = matrix()) {
+    
+    # constructor
+    trix <- NULL
+    set <- function(y) {
+        x <<- y
+        trix <<- NULL
+    }
+    # methods
+    get <- function() x
+    set.trix<-function(solve) trix <<- solve
+    get.trix<-function() trix
+    list(set = set, get = get,
+    set.trix = set.trix,
+    get.trix = get.trix)
 }
 
+# solver function, returns inverse of the matrix
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+    
+    # get matrix
+    m <- x$get.trix()
+    # if cached, return cache
+    if (!is.null(m)) {
+        message("getting cached matrix")
+        return(m)
+    }
+    # else get and solve for the inverse of x, then return inverse
+    trix <- x$get()
+    m <- solve(trix, ...)
+    x$set.trix(m)
+    m
 }
